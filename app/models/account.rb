ad54bf6acbb2e8053a104b5cbabfd7a2cc188bb7
charleshug <charleshug@gmail.com>
2023-02-05 10:14:27 -0800

    added validations to account model

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,2 +1,6 @@
 class Account < ApplicationRecord
+  validates :balance, presence: true
+  validates :name, presence: true
+  validates :on_budget, inclusion: { in: [true, false] }
+  validates :closed, inclusion: { in: [true, false] }
 end
