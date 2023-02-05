dfb33eacd62ae5f6433a6c633bfd9c0ea5994397
charleshug <charleshug@gmail.com>
2023-02-05 12:46:47 -0800

    make helper methods available to all controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :get_account_info
+  include BudgetsHelper
+  include TrxesHelper
+
+  def get_account_info
+    @accounts = Account.not_budget_type
+    @accounts_onBudget = @accounts.on_budget.open
+    @accounts_offBudget = @accounts.off_budget.open
+    @accounts_closed = @accounts.closed
+  end
 end
