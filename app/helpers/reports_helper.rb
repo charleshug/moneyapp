b33275dd21748dcccb5ac5fc4283906e2fc7aab0
charleshug <charleshug@gmail.com>
2023-02-05 12:49:39 -0800

    update reports helper methods

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,2 +1,8 @@
 module ReportsHelper
+
+  def get_net_worth
+    #builds a hash of Period => Sum, then .maps so each month represents the rolling balance, then reduces it from hash to array
+    sum=0
+    Trx.registerItems.group_by_month(:date, format: '%b %Y').sum(:amount).map { |x,y| { x => (sum += y)} }.reduce({}, :merge)
+  end
 end
