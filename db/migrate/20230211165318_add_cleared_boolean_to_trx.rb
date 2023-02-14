fa1aef247ccb447251533e3fd98fbd57bdfa15a3
charleshug <charleshug@gmail.com>
2023-02-14 11:46:38 -0800

    moved add_index to be within def change to fix an error where program wouldn't work

--- db/migrate/20230211165318_add_cleared_boolean_to_trx.rb
+++ db/migrate/20230211165318_add_cleared_boolean_to_trx.rb
@@ -1,6 +1,6 @@
 class AddClearedBooleanToTrx < ActiveRecord::Migration[7.0]
   def change
     add_column :trxes, :cleared, :boolean, null: false, default: false
+    add_index :trxes, :cleared
   end
-  add_index :trxes, :cleared
 end
