29b8f4e18f65e9d137c09664ec926d9714125a22
charleshug <charleshug@gmail.com>
2023-04-16 17:18:52 -0700

    added tom-select category to trx form

---
+++ app/javascript/controllers/category_select_controller.js
@@ -0,0 +1,25 @@
+import { Controller } from "@hotwired/stimulus"
+import TomSelect from "tom-select"
+
+// Connects to data-controller="vendor-select"
+export default class extends Controller {
+
+  connect() {
+    this.initTomSelect()
+  }
+
+  disconnect() {
+    if (this.select) {
+      this.select.destroy()
+    }
+  }
+
+  initTomSelect() {
+    var settings = {
+      plugins: ['clear_button'],
+      selectOnTab: true,
+    }
+
+    this.select = new TomSelect(this.element, settings)
+  }
+}
