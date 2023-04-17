35393f666b91d8c7fad279b52044207d15ecc8df
charleshug <charleshug@gmail.com>
2023-04-16 17:13:03 -0700

    corrected comment

--- app/javascript/controllers/account_select_controller.js
+++ app/javascript/controllers/account_select_controller.js
@@ -1,11 +1,10 @@
 import { Controller } from "@hotwired/stimulus"
 import TomSelect from "tom-select"
 
-// Connects to data-controller="vendor-select"
+// Connects to data-controller="account-select"
 export default class extends Controller {
 
   connect() {
-    this.element.setAttribute("autocomplete", "random");
     this.initTomSelect()
   }
 
