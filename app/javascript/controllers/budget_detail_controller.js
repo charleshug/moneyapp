213f410169f0bbbf347352386559341a2e72a604
charleshug <charleshug@gmail.com>
2023-05-12 13:24:51 -0700

    removed unneeded stimulus target budget-detail

--- app/javascript/controllers/budget_detail_controller.js
+++ app/javascript/controllers/budget_detail_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="budget-detail"
 export default class extends Controller {
-  static targets = ['parent','childGroup']
+  static targets = ['childGroup']
   
   toggle() {
     this.childGroupTarget.classList.toggle('hidden')
