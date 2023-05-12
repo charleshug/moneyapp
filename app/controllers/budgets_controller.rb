0d9aa974b87b73e8d368b4332a0edcd0ba7c7a2a
charleshug <charleshug@gmail.com>
2023-05-12 13:06:53 -0700

    removed references to desktop views

--- app/controllers/budgets_controller.rb
+++ app/controllers/budgets_controller.rb
@@ -2,7 +2,6 @@ class BudgetsController < ApplicationController
   def index
     if params[:period]
       @given_date = (params[:period] + "-01").to_date.beginning_of_month
-      #@given_date = params[:period].to_date.beginning_of_month
       session[:period]=@given_date
     elsif session[:period]
       @given_date = session[:period].to_date
@@ -12,20 +11,7 @@ class BudgetsController < ApplicationController
     end
 
     session[:page]="Budgets"
-    
-    # @current_year = @given_date.year
-    # @current_month = @given_date.month
 
-    @previous_date = @given_date << 1
-    # @previous_year = @previous_date.year
-    # @previous_month = @previous_date.month
-
-    @next_date = @given_date >> 1
-    # @next_year = @next_date.year
-    # @next_month = @next_date.month
-
-    #@previousData = getBudgetData(@previous_date)
     @currentData = getBudgetData(@given_date)
-    #@futureData = getBudgetData(@next_date)
   end
 end
